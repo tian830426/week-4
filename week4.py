042aee26fb4ad5dd5a463cc438f1bd2d94bbcfc9
tian830426 <105027318+tian830426@users.noreply.github.com>
2022-10-16 22:38:44 +0800

    123
    123

--- week4.py
+++ week4.py
@@ -4,31 +4,34 @@ from flask import Flask,request,render_template,redirect,session,url_for
 app = Flask(__name__)
 
 #設定 session 密鑰
-app.secret_key='tian0426'
+app.secret_key='tian12345'
 
 @app.route('/')
 def index():
     return render_template("signin.html")
 
+#路由-後端-路由-前端
 #account=參數名稱/ acc=資料
 #session ['參數名稱‘] = ‘資料’
-#釐清哪些狀況下會跳轉到成功頁面 1.test 2.曾經有登入紀錄 3.保持登入頁面
+#釐清哪些狀況下會跳轉到成功頁面 1.輸入test 2.曾經有登入紀錄 3.保持登入頁面
+#request 要求物件 / request.args.get  
 #redirect 導向另一個路徑/也可直接傳送網址
+#render_template 導向前端
 @app.route('/signin',methods=['POST'])
 def signin(): 
     acc = request.form['account']
-    session['account']= acc
+    # session['account']= acc
     psw = request.form["password"]
-    session['password']= psw
+    # session['password']= psw
     if (acc == "test" and psw == "test") or session['enter'] == 'open':
         session['enter'] = 'open'
         return redirect(url_for("member")) 
-    elif acc == '' or psw == '' :
+    elif acc == '' and psw == '' :
         return redirect('http://127.0.0.1:3000/error?message=請輸入帳號密碼') 
     else:
         return redirect(url_for("error")) 
 
-#跳轉到memeber的路由後，如果紀錄保持打開，便會渲染到member.html ，反之跳轉到首頁。 
+#跳轉到memeber的路由後，如果紀錄保持存在，便會渲染到member.html ，反之跳轉到首頁。 
 @app.route('/member')
 def member():
     if session['enter'] == 'open':
